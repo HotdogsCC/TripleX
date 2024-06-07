a92d1bb020318facc3e107fb557f1be7beeb2575
Charlie Campbell <chacha9669@hotmail.com>
2024-06-08 01:36:16 +1000

    used protection (const)

---
+++ TripleX.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello. You are a secret agent working for H.W.";
+    std::cout << std::endl;
+    std::cout << "Your task is simple, crack the hardrive into BJ Solutions.";
+    std::cout << std::endl;
+
+    const int a = 4;
+    const int b = 5;
+    const int c = 6;
+
+    const int sum = a + b + c;
+    const int product = a * b * c;
+
+    std::cout << sum << std::endl;
+    std::cout << product;
+
+    return 0;
+}
