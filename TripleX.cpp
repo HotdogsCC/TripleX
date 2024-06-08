2f32db41efed4649eaa8b160d06d172226196107
Charlie Campbell <chacha9669@hotmail.com>
2024-06-08 17:14:25 +1000

    while loops

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 
-int main()
+void PrintIntroduction(int Difficulty)
 {
     // Prints some messages to the terminal
-    std::cout << "Hello. You are a secret agent working for H.W.";
-    std::cout << std::endl;
-    std::cout << "Your task is simple, crack the hardrive into BJ Solutions.";
-    std::cout << std::endl;
-    std::cout << std::endl;
+    std::cout << "\n\nHello. You are a secret agent working for H.Watson.\n";
+    std::cout << "Your task is simple, crack the hardrive owning to B.Joosse.\n";
+    std::cout << "This hard drive has a difficulty rating of " << Difficulty << "\n";
+}
+
+bool PlayGame(int Difficulty)
+{
+    PrintIntroduction(Difficulty);
 
     // Declare 3 number code
     const int CodeA = 4;
@@ -18,14 +21,13 @@ int main()
     const int CodeProduct = CodeA * CodeB * CodeC;
 
     // Prints sum and product to the terminal
-    std::cout << "There are three numbers in the code" << std::endl;
-    std::cout << "The codes add-up to: " << CodeSum << std::endl;
-    std::cout << "The product of the codes result in: " << CodeProduct << std::endl;
+    std::cout << "There are three numbers in the code.\n";
+    std::cout << "The codes add-up to: " << CodeSum << "\n";
+    std::cout << "The product of the codes result in: " << CodeProduct << "\n";
 
+    // Store player guess
     int GuessA, GuessB, GuessC;
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
@@ -33,10 +35,29 @@ int main()
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
         std::cout << "You win!!!";
+        return true;
     }
     else
     {
         std::cout << "You lose!!!";
+        return false;
+    }
+}
+
+int main()
+{
+    int LevelDifficulty = 1;
+    while (true)
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear();
+        std::cin.ignore();
+
+        if (bLevelComplete)
+        {
+            ++LevelDifficulty;
+        }
+        
     }
 
     return 0;
