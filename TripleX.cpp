632dc677bb8a0d17ef50d99d5e79601aaaa4bc8a
Charlie Campbell <chacha9669@hotmail.com>
2024-06-08 23:44:27 +1000

    finished game

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <ctime>
 
 void PrintIntroduction(int Difficulty)
 {
     // Prints some messages to the terminal
     std::cout << "\n\nHello. You are a secret agent working for H.Watson.\n";
     std::cout << "Your task is simple, crack the hardrive owning to B.Joosse.\n";
-    std::cout << "This hard drive has a difficulty rating of " << Difficulty << "\n";
+    std::cout << "\n This hard drive has a difficulty rating of " << Difficulty << "\n";
 }
 
 bool PlayGame(int Difficulty)
@@ -13,15 +14,15 @@ bool PlayGame(int Difficulty)
     PrintIntroduction(Difficulty);
 
     // Declare 3 number code
-    const int CodeA = 4;
-    const int CodeB = 5;
-    const int CodeC = 6;
+    const int CodeA = rand() % Difficulty + Difficulty;
+    const int CodeB = rand() % Difficulty + Difficulty;
+    const int CodeC = rand() % Difficulty + Difficulty;
 
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
 
     // Prints sum and product to the terminal
-    std::cout << "There are three numbers in the code.\n";
+    std::cout << "\nThere are three numbers in the code.\n";
     std::cout << "The codes add-up to: " << CodeSum << "\n";
     std::cout << "The product of the codes result in: " << CodeProduct << "\n";
 
@@ -34,20 +35,22 @@ bool PlayGame(int Difficulty)
 
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "You win!!!";
+        std::cout << "\nGood stuff, that seemed to have cracked that drive.";
         return true;
     }
     else
     {
-        std::cout << "You lose!!!";
+        std::cout << "\nFuck, that wasn't it. Let's try again.";
         return false;
     }
 }
 
 int main()
 {
+    srand(time(NULL));
     int LevelDifficulty = 1;
-    while (true)
+    const int MaxDifficulty = 5;
+    while (LevelDifficulty <= MaxDifficulty) // Loop game until all levels are completed
     {
         bool bLevelComplete = PlayGame(LevelDifficulty);
         std::cin.clear();
@@ -59,6 +62,8 @@ int main()
         }
         
     }
+
+    std::cout << "GG you win.";
 
     return 0;
 }
